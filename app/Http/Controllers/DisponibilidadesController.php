711815cf50fcc99076e44bd7297e87207643a584
ManuelG0408 <L202207045@vbravo.tecnm.mx>
2024-10-24 12:44:50 -0600

    changes.manuel

--- app/Http/Controllers/DisponibilidadesController.php
+++ app/Http/Controllers/DisponibilidadesController.php
@@ -13,4 +13,40 @@ class DisponibilidadesController extends Controller
             'disponibilidades' => $disponibilidades
         ]);
     }
+
+    public function create()
+    {
+        return view('admin.disponibilidades.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'desc_disponibilidad' => 'required|string|max:255',
+        ]);
+
+        Disponibilidad::create($request->all());
+        return redirect()->route('disponibilidades.index')->with('success', 'Disponibilidad creada con éxito.');
+    }
+
+    public function edit(Disponibilidad $disponibilidad)
+    {
+        return view('disponibilidades.edit', compact('disponibilidad'));
+    }
+
+    public function update(Request $request, Disponibilidad $disponibilidad)
+    {
+        $request->validate([
+            'desc_disponibilidad' => 'required|string|max:255',
+        ]);
+
+        $disponibilidad->update($request->all());
+        return redirect()->route('disponibilidades.index')->with('success', 'Disponibilidad actualizada con éxito.');
+    }
+
+    public function destroy(Disponibilidad $disponibilidad)
+    {
+        $disponibilidad->delete();
+        return redirect()->route('disponibilidades.index')->with('success', 'Disponibilidad eliminada con éxito.');
+    }
 }
